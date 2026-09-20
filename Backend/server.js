4e1b230c2f786059bb1aab185c66f79d166586a1
Aparna Nale <aparna.nale099@gmail.com>
2026-09-20 10:14:28 +0530

    update

--- Backend/server.js
+++ Backend/server.js
@@ -9,7 +9,10 @@ connectDB();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: process.env.CLIENT_URL || "http://localhost:5173",
+  credentials: true,
+}));
 app.use(express.json({ limit: "5mb" })); 
 // Routes
 app.use("/api/auth", require("./routes/authRoutes"));
